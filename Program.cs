0e56a4f8e9c62882f8b76c485c020bece851630b
L-WF <zenmegao1993@gmail.com>
2014-08-11 11:22:46 +0800

    处理文件
    在编译出来的exe文件目录下，创建begin和end文件夹，将所有一级文件夹放到begin文件夹下，运行exe，结果：所有一级文件夹中的相同名字会被整理出来，在end文件夹下创建相同名字的文件夹，然后该名字的所有文件会被复制到对应的文件夹，并从0开始重命名这些文件！

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace DealFiles
+{
+    class Program
+    {
+        static DirectoryInfo begin = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + "\\begin");
+        static DirectoryInfo end = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + "\\end");
+        static DirectoryInfo[] dirs = begin.GetDirectories();
+        static List<string> beginFileNames = new List<string>();
+        static List<string> repeatFileNames = new List<string>();
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("                              林伟丰——18826418792");
+            Console.WriteLine("进行中……");
+            GetBeginNames();
+            GetRepeatNames();
+            CreatSubDirs();
+            MoveFiles();
+            Console.WriteLine("已完成，按任意键退出……");
+            Console.ReadKey();
+        }
+
+        static void GetBeginNames()
+        {
+            foreach (var dir in dirs)
+            {
+                FileInfo[] files = dir.GetFiles();
+                foreach (var file in files)
+                {
+                    beginFileNames.Add(file.Name);
+                }
+            }
+        }
+
+        static void GetRepeatNames()
+        {
+            int count = beginFileNames.Count;
+            for( int i=0 ; i<count ; i++ )
+            {
+                for (int j = i+1; j<count ; j++)
+                {
+                    if( string.Compare(beginFileNames[i],beginFileNames[j]) == 0 )
+                    {
+                        AddRepeatNames(beginFileNames[i]);
+                        break;
+                    }
+                }
+            }
+        }
+
+        static void AddRepeatNames(string s)
+        {
+            foreach (var repeatFileName in repeatFileNames)
+            {
+                if ( string.Compare(repeatFileName,s) == 0 )
+                {
+                    return;
+                }
+            }
+            repeatFileNames.Add(s);
+        }
+
+        static void CreatSubDirs()
+        {
+            foreach (var repeatFileName in repeatFileNames)
+            {
+                int length = repeatFileName.IndexOf(".");
+                end.CreateSubdirectory(repeatFileName.Substring(0, length));
+            }
+        }
+
+        static void MoveFiles()
+        {
+            foreach (var dir in dirs)
+            {
+                FileInfo[] Files = dir.GetFiles();
+                foreach (var File in Files)
+                {
+                    foreach (var repeatFileName in repeatFileNames)
+                    {
+                        if( string.Compare(File.Name,repeatFileName) == 0 )
+                        {
+                            int length = repeatFileName.IndexOf(".");
+                            DirectoryInfo targetDir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + "\\end\\" + repeatFileName.Substring(0, length));
+                            string copyName = targetDir.GetFiles().Count() + File.Extension;
+                            System.IO.File.Copy(File.FullName,targetDir.FullName+"\\"+copyName);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
